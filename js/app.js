20f9012de15e42c1fa3e23f2f66318643287dcd0
sea5sky2 <sea5sky2@gmail.com>
2016-07-26 19:22:02 +0900

    20160726:1919_예은

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova', 'ngAnimate', 'ui.router', 'ngFileUpload'])
+
 
   .constant('HOST', 'http://localhost:8080')
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -24,7 +26,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+
+  .config(function($stateProvider, $urlRouterProvider, USER_ROLES) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -33,12 +36,72 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $stateProvider
 
   // setup an abstract state for the tabs directive
+
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    })
     .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
+    .state('tab.userSharedPage', {
+      url: '/userSharedPage',
+      views:{
+        'tab-userSharedPage':{
+          templateUrl: 'templates/tab-userSharedPage.html',
+          controller: 'RelatedMemberListCtrl'
+        }
+      }
+    })
+    .state('tab.api', {
+      url: '/api',
+      views:{
+        'tab-api':{
+          templateUrl: 'templates/tab-api.html',
+          controller: 'ApiCtrl'
+        }
+      }
+      ,
+      data: {
+        authorizedRoles: [USER_ROLES.admin]
+      }
+    })
+
+    .state('join', {
+      url: '/join',
+      templateUrl: 'templates/join.html',
+      controller: 'JoinCtrl'
+    })
+
+    .state('tab.member', {
+      url: '/member',
+      views: {
+        'tab-member': {
+          templateUrl: 'templates/tab-member.html',
+          controller: 'MemberCtrl'
+        }
+      }
+    })
+    .state('settings', {
+      url: '/settings',
+      templateUrl: 'templates/settings.html',
+      controller: 'SettingCtrl'
+    })
+    .state('searchApi', {
+      url: '/searchApi',
+      templateUrl:'templates/searchApi.html',
+      controller:'searchApiCtrl'
+    })
+    .state('userSharedPage-detail', {
+      url: '/userSharedPage-detail',
+      templateUrl: 'templates/userSharedPage-detail.html',
+      controller:'SharedPageDetailCtrl'
+    })
+  //**************************************** 예은 추가분****************************************
     .state('tab.cosmetics', { //화장품전체목록
       url: '/cosmetics',
       views: {
@@ -68,18 +131,28 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
+  //****************************************예은추가분 끝****************************************
+  ;
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider
+    .otherwise('/login');
+})
 
-    .state('tab.login', { //혜윤이 로그인
-      url: '/login',
-      views: {
-        'tab-login': {
-          templateUrl: 'templates/tab-login.html',
-          controller: 'LoginCtrl'
+  .run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+    $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
+      if ('data' in next && 'authorizedRoles' in next.data) {
+        var authorizedRoles = next.data.authorizedRoles;
+        if (!AuthService.isAuthorized(authorizedRoles)) {
+          event.preventDefault();
+          $state.go($state.current, {}, {reload: true});
+          $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
         }
       }
+      // if (!AuthService.isAuthenticated()) {
+      //   if (next.name !== 'login') {
+      //     event.preventDefault();
+      //     $state.go('login');
+      //   }
+      // }
     });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/login');
-
-});
+  });
--- js/services.js
+++ js/services.js
@@ -1,19 +1,188 @@
 angular.module('starter.services', [])
 
-  .service('LoginSvc', function($http, HOST) {   //해니꺼 로그인
-
-    this.checkLoginInfo = function(data) {
+  .service('HttpSvc', function($http, HOST) {
+    //Member table에 등록된 모든 정보 가져옴
+    this.getMemberList = function () {
       return $http({
-        url: HOST + '/api/login',
+        url: HOST + '/api/memberList',
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+      });  };
+
+      //화장품 정보 등록
+    this.addCosmetics = function (query) {
+      return  $http({
+        url: HOST + '/api/cosmetics',
         method: 'POST',
-        data: data,
+        data: query,
+        headers: {'Content-Type': 'application/json'}
+      });  };
+
+      //Daum api로 검색한 화장품 정보 가져옴 (관리자모드)
+    this.getSearchResultList = function (query) {
+      return $http({
+        url : HOST + '/api/daumSearch',
+        method: 'POST',
+        data: query,
+        headers: {'Content-Type': 'application/json'}
+      });  };
+
+      //사용자와 비슷한 화장품 갖고있는 회원 상위 3개 리스트 가져옴
+      this.getRelatedMemberList = function (query) {
+        return $http({
+          url : HOST + '/api/getRelatedMemberList',
+          method: 'POST',
+          data: query,
+          headers: {'Content-Type': 'application/json'}
+        });  };
+
+        //추천수 ++
+        this.updateMemberStar = function (query) {
+          return $http({
+            url : HOST + '/api/updateMemberStar',
+            method: 'POST',
+            data: query,
+            headers: {'Content-Type': 'application/json'}
+          }); };
+
+          //추천수 높은 순으로 회원정보 상위3개 리스트 가져옴
+        this.getHighRankList = function (query) {
+          return $http({
+            url : HOST + '/api/getHighRankList',
+            method: 'POST',
+            data: query,
+            headers: {'Content-Type': 'application/json'}
+          });   };
+
+    // MemberIndex 가 가지고있는 화장품들 데이터 가져오기
+    this.getMyCosmeticsByMemberIndex= function (member_index) {
+      return $http({
+        url: HOST + '/api/mycostableList',
+        method: 'POST',
+        data: member_index,
         headers: {'Content-Type': 'application/json'}
       });
     };
+
   })
 
-  .service('HttpSvc', function($http, HOST) {  //화장품목록 가져오기
+  //Authentication 을 위한 부분. 로그인&로그아웃 처리 등
+  .service('AuthService', function($q, $http, USER_ROLES, HOST) {
+    var LOCAL_TOKEN_KEY = 'yourTokenKey';
+    var id = '';
+    var index = '';
+    var password = '';
+    var image = '';
+    var isAuthenticated = false;
+    var role = '';
+    var authToken;
 
+    function loadUserCredentials() {
+      var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+      if (token) {
+        useCredentials(token);
+      }
+    }
+
+    function storeUserCredentials(token) {
+      window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
+      useCredentials(token);
+    }
+
+    function useCredentials(token) {
+      index = token.split(',')[0];
+      id = token.split(',')[1];
+      password = token.split(',')[2];
+      image = token.split(',')[3];
+      isAuthenticated = true;
+      authToken = token;
+      if (id == 'admin') {
+        role = USER_ROLES.admin
+      }
+      else{
+        role = USER_ROLES.public
+      }
+
+      // // Set the token as header for your requests!
+      $http.defaults.headers.common['X-Auth-Token'] = token;
+    }
+
+    function destroyUserCredentials() {
+      authToken = undefined;
+      index='';
+      id = '';
+      password='';
+      image='';
+      isAuthenticated = false;
+      $http.defaults.headers.common['X-Auth-Token'] = undefined;
+      window.localStorage.removeItem(LOCAL_TOKEN_KEY);
+    }
+
+    var login = function(id, pw) {
+      return $q(function(resolve, reject) {
+        var data={id : id, password: pw};
+       var request = $http({
+          url: HOST + '/api/login',
+          method: 'POST',
+          data: data,
+          headers: {'Content-Type': 'application/json'}
+        });
+        request.success(function (values) {
+          if (values != null && values != "") {
+            storeUserCredentials(values.member_index + ',' + id + ',' + pw + ',' + values.image + ',yourServerToken');
+            resolve('Login success.');
+          } else {
+            reject('Login Failed.');
+          }
+        }).error(function (value, status) {
+          alert(value + " , " +  status);
+        });
+      });
+    };
+
+    var logout = function() {
+      destroyUserCredentials();
+    };
+
+    var isAuthorized = function(authorizedRoles) {
+      if (!angular.isArray(authorizedRoles)) {
+        authorizedRoles = [authorizedRoles];
+      }
+      return (isAuthenticated && authorizedRoles.indexOf(role) !== -1);
+    };
+
+    loadUserCredentials();
+
+    return {
+      login: login,
+      logout: logout,
+      isAuthorized: isAuthorized,
+      isAuthenticated: function() {return isAuthenticated;},
+      id: function() {return id;},
+      role: function() {return role;},
+      index: function () {return index;},
+      image: function () {return image;}
+    };
+  })
+
+  .factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
+    return {
+      responseError: function (response) {
+        $rootScope.$broadcast({
+          401: AUTH_EVENTS.notAuthenticated,
+          403: AUTH_EVENTS.notAuthorized
+        }[response.status], response);
+        return $q.reject(response);
+      }
+    };
+  })
+
+  .config(function ($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+  })
+
+/************************************예은 코드*****************************************/
+  .service('HttpSvc2', function ($http, HOST) {
     this.getCosmeticsList = function () {
       return $http({
         url: HOST + '/api/cosmeticsList',
@@ -21,9 +190,6 @@ angular.module('starter.services', [])
         headers: {'Content-Type': 'application/json'}
       });
     };
-  })
-
-  .service('HttpSvc2', function ($http, HOST) {
 
     this.getSearch = function (query) { //화장품 검색하기,
       return $http({
@@ -43,15 +209,6 @@ angular.module('starter.services', [])
       });
     };
 
-    this.deleteInterest = function (interest) { //관심리스트등록
-      return $http({
-        url: HOST + '/api/deleteInterest',
-        method: 'POST',
-        data: interest,
-        headers: {'Content-Type': 'application/json'}
-      });
-    };
-
     this.getInterestList = function (query) {  //관심리스트 불러오기
       return $http({
         url: HOST + '/api/interestList',
@@ -60,5 +217,7 @@ angular.module('starter.services', [])
         headers: {'Content-Type': 'application/json'}
       });
     };
+  })
+/************************************예은 코드끝*****************************************/
+;
 
-});
